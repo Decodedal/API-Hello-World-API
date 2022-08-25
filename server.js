75710ec84f128922933655d2d653428d03e45725
Dallas Palumbo <palumbodallas@gmail.com>
2022-08-24 20:30:19 -0400

    success

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
+
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
+
+//Languages roughter
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
 
 // ROUTES
 app.get('/', (req, res) => {
